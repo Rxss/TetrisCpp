a3f1cb93d79db26d67b163e2bbc4275185b8978d
Rxss <rossarmo99@gmail.com>
2025-07-19 18:17:25 +0100

    created file for storing all 7 diferent types of pieces in 5x5 matrices, arrays for the displacement of the piece to the position where it is first drawn on the board

--- pieces.h
+++ pieces.h
@@ -1,1 +1,18 @@
-#pragma once
+// Ross A
+// 19/07/2025
+
+#ifndef _PIECES_
+#define _PIECES_
+
+
+class Pieces {
+public:
+
+	int getBlockType(int piece, int rotation, int x, int y);
+	int getXInitialPosition(int piece, int rotation);
+	int getYInitialPosition(int piece, int rotation);
+};
+
+
+
+#endif // _PIECES_
