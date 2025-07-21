544b3439425f7f93c986af5033cd1b99a0e593a9
Rxss <rossarmo99@gmail.com>
2025-07-21 21:08:55 +0100

    finished board functions, finsihed game.h, added IO.h

--- Board.cpp
+++ Board.cpp
@@ -43,3 +43,53 @@ void Board::DeleteLine(int y)
 		}
 	}
 }
+
+void Board::DeletePossibleLines()
+{
+	for (int j = 0; j < BOARD_HEIGHT; j++) {
+		int i = 0;
+		while (i < BOARD_WIDTH) {
+			if (board[i][j] != POS_FILLED) break;
+			i++;
+		}
+		if (i == BOARD_WIDTH) DeleteLine(j);
+	}
+}
+
+bool Board::IsFreeBlock(int x, int y)
+{
+	if (board[x][y] == POS_FREE) 
+		return true; 
+	else return false;
+}
+
+int Board::GetXPosInPixels(int pos)
+{
+	return ((BOARD_POSITION - (BLOCK_SIZE * (BOARD_WIDTH / 2))) + (pos * BLOCK_SIZE));
+}
+
+int Board::GetYPosInPixels(int pos)
+{
+	return ((ScreenHeight - (BLOCK_SIZE)) * (BOARD_HEIGHT) + (pos * BLOCK_SIZE));
+}
+
+bool Board::IsPossibleMovement(int x, int y, int piece, int rotation)
+{
+	for (int i1 = x,int i2 = 0; i1 < x + PIECE_BLOCKS; i1++, i2++) {
+		for (int j1 = y, int j2 = 0; j1 < y + PIECE_BLOCKS; i1++, i2++) {
+			if (i1 < 0 || i1 > BOARD_WIDTH - 1 || j1 > BOARD_HEIGHT - 1) {
+				if (Pieces->GetBlockType(piece, rotation, j2, i1) != 0) {
+					return 0;
+				}
+
+				if (j1 >= 0) {
+					if ((Pieces->GetBlockType(piece, rotation, j2, i2) != 0) && (!IsFreeBlock(i1, j1))) {
+						return false;
+
+					}
+				}
+			}
+		}
+		return true;
+	}
+}
---
+++ Game.h
@@ -0,0 +1,19 @@
+#ifndef _GAME_
+#define _GAME_
+
+#include "Board.h"
+#include "pieces.h"
+#include "IO.h"
+#include <time.h>
+
+#define WAIT_TIME 700 // Number of Miliseconds that the piece remains before going 1 block down
+
+class Game {
+public:
+	Game(Board *board, Pieces *Pieces, IO *IO
+private:
+};
+
+
+
+#endif
