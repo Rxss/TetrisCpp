699f08c42189215d85fe672dddc436d51a2afb11
Rxss <rossarmo99@gmail.com>
2025-07-21 21:09:57 +0100

    same commit, forgot to save

--- Game.h
+++ Game.h
@@ -10,8 +10,27 @@
 
 class Game {
 public:
-	Game(Board *board, Pieces *Pieces, IO *IO
+	Game(Board* board, Pieces* Pieces, IO* IO, int ScreenHeight);
+
+	void DrawScene();
+	void CreateNewPiece();
+
+	int posX, posY; //position of the falling piece
+	int piece, rotation; // Kind and rotation of the falling piece
+
 private:
+	int mScreenHeight; // Screen height in pixels
+	int mNextPosX, mNextPosY; // Position of the next piece
+	int mNextPiece, mNextRotation; // Kind and rotation of the next piece
+
+	Board* mBoard;
+	Pieces* mPieces;
+	IO* mIO;
+
+	int GetRand(int pA, int pB);
+	void InitGame();
+	void DrawPiece(int pX, int pY, int pPiece, int pRotation);
+	void DrawBoard();
 };
 
 
